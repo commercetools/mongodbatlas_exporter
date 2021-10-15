ce3ef244124fd167d6b08ddca93dfcb6abcf85f5
Fulton Byrne <fulton.byrne@commercetools.com>
2021-10-15 16:15:32 -0400

    lint

--- collector/common_test.go
+++ collector/common_test.go
@@ -3,7 +3,6 @@ package collector
 import (
 	"mongodbatlas_exporter/measurer"
 	"mongodbatlas_exporter/model"
-	m "mongodbatlas_exporter/model"
 	"os"
 	"testing"
 
@@ -97,9 +96,9 @@ func getExpectedDescs() []*prometheus.Desc {
 	return result
 }
 
-func getGivenMeasurementMetadata() map[m.MeasurementID]*m.MeasurementMetadata {
-	return map[m.MeasurementID]*m.MeasurementMetadata{
-		m.NewMeasurementID("DISK_PARTITION_IOPS_READ", "SCALAR_PER_SECOND"): {
+func getGivenMeasurementMetadata() map[model.MeasurementID]*model.MeasurementMetadata {
+	return map[model.MeasurementID]*model.MeasurementMetadata{
+		model.NewMeasurementID("DISK_PARTITION_IOPS_READ", "SCALAR_PER_SECOND"): {
 			Name:  "DISK_PARTITION_IOPS_READ",
 			Units: "SCALAR_PER_SECOND",
 		},
